3432ed4660ce4a4c214e3ea8bfa42524ebbc1817
vaishali0707 <vaishali.tiwari0412@gmail.com>
2020-07-17 16:06:35 +0530

    updated

--- BookStore/src/main/java/com/cg/bookStore/dao/BookStoreDaoImplementation.java
+++ BookStore/src/main/java/com/cg/bookStore/dao/BookStoreDaoImplementation.java
@@ -3,8 +3,10 @@ package com.cg.bookStore.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.cg.bookStore.entities.Admin;
 import com.cg.bookStore.entities.CustomerInformation;
 import com.cg.bookStore.entities.CustomerReview;
